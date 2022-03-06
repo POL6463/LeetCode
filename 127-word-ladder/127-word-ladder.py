af85cf9841d8d411f593f67817b92203f8e77d64
Minwoong Kim <61047392+POL6463@users.noreply.github.com>
2022-03-05 19:47:48 -0800

    Time: 162 ms (81.75%), Space: 18.3 MB (21.37%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+        
+        L = len(beginWord)
+        
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word)
+                
+        queue = collections.deque([(beginWord, 1)])
+        visited = {beginWord: True}
+        while queue:
+            current_word, level = queue.popleft()
+            for i in range(L):
+                # Intermediate words for current word
+                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+
+                # Next states are all the words which share the same intermediate state.
+                for word in all_combo_dict[intermediate_word]:
+                    # If at any point if we find what we are looking for
+                    # i.e. the end word - we can return with the answer.
+                    if word == endWord:
+                        return level + 1
+                    # Otherwise, add it to the BFS Queue. Also mark it visited
+                    if word not in visited:
+                        visited[word] = True
+                        queue.append((word, level + 1))
+                all_combo_dict[intermediate_word] = []
+        return 0
