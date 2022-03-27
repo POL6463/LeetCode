8e9fb58a7a093a5d7f7ededc68af08f7c7f44eab
Kevin Minwoong Kim <61047392+POL6463@users.noreply.github.com>
2022-03-27 22:39:30 +0900

    Time: 190 ms (20.01%), Space: 17 MB (44.54%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,50 +1,26 @@
-class GNode(object):
-    """  data structure represent a vertex in the graph."""
-    def __init__(self):
-        self.inDegrees = 0
-        self.outNodes = []
-
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        from collections import defaultdict, deque
-        # key: index of node; value: GNode
-        graph = defaultdict(GNode)
-
-        totalDeps = 0
-        for relation in prerequisites:
-            nextCourse, prevCourse = relation[0], relation[1]
-            graph[prevCourse].outNodes.append(nextCourse)
-            graph[nextCourse].inDegrees += 1
-            totalDeps += 1
-
-        # we start from courses that have no prerequisites.
-        # we could use either set, stack or queue to keep track of courses with no dependence.
-        nodepCourses = deque()
-        for index, node in graph.items():
-            if node.inDegrees == 0:
-                nodepCourses.append(index)
-
-        removedEdges = 0
-        while nodepCourses:
-            # pop out course without dependency
-            course = nodepCourses.pop()
-
-            # remove its outgoing edges one by one
-            for nextCourse in graph[course].outNodes:
-                graph[nextCourse].inDegrees -= 1
-                removedEdges += 1
-                # while removing edges, we might discover new courses with prerequisites removed, i.e. new courses without prerequisites.
-                if graph[nextCourse].inDegrees == 0:
-                    nodepCourses.append(nextCourse)
-
-        if removedEdges == totalDeps:
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = { i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+            
+            
+        visitSet = set()
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if preMap[crs] == []:
+                return True
+            
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+            visitSet.remove(crs)
+            preMap[crs] = []
+            
             return True
-        else:
-            # if there are still some edges left, then there exist some cycles
-            # Due to the dead-lock (dependencies), we cannot remove the cyclic edges
-            return False
+        
+        
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+        return True
