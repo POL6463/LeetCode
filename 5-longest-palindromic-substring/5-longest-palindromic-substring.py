a8615df371345a8c2669fc833949d2c4f0f73e40
Kevin Kim <61047392+POL6463@users.noreply.github.com>
2022-06-27 15:47:39 -0700

    Time: 723 ms (90.69%), Space: 13.9 MB (60.50%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,19 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        self.maxlen = 0
-        self.start = 0
-
-        for i in range(len(s)):
-            self.expandFromCenter(s,i,i)
-            self.expandFromCenter(s,i,i+1)
-        return s[self.start:self.start+self.maxlen]
+        if len(s) <= 1:
+            return s
+        start = end = 0
+        length = len(s)
+        for i in range(length):
+            max_len_1 = self.get_max_len(s, i, i + 1)
+            max_len_2 = self.get_max_len(s, i, i)
+            max_len = max(max_len_1, max_len_2)
+            if max_len > end - start:
+                start = i - (max_len - 1) //2
+                end = i + max_len //2
+        return s[start: end+1]
     
-
-    def expandFromCenter(self,s,l,r):
-        while l > -1 and r < len(s) and s[l] ==s[r]:
-            l -= 1
-            r += 1
-
-        if self.maxlen < r-l-1:
-            self.maxlen = r-l-1
-            self.start = l + 1
+    def get_max_len(self, s, left, right):
+        length = len(s)
+        i = 1
+        max_len = 0
+        while left >= 0 and right < length and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left -1
