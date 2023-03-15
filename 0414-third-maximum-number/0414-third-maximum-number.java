ea267f7e166ceac1f253c7d170d827411b74d54e
Kevin Kim <61047392+POL6463@users.noreply.github.com>
2023-03-14 23:26:36 -0700

    Time: 4 ms (46.52%), Space: 41.8 MB (85.65%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> sortedNums = new TreeSet<Integer>();
+        
+        for (int num : nums) {
+            if (sortedNums.contains(num)) {
+                continue;
+            }
+            
+            if (sortedNums.size() == 3) {
+                if (sortedNums.first() < num) {
+                    sortedNums.pollFirst();
+                    sortedNums.add(num);
+                }
+            } else {
+                sortedNums.add(num);
+            }
+        }
+        
+        if (sortedNums.size() == 3) {
+            return sortedNums.first();
+        }
+        
+        return sortedNums.last();
+    }
+}
