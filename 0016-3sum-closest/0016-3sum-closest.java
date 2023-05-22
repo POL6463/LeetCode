761886029a443d6ab0c947662e967a49056d3682
Kevin Kim <61047392+POL6463@users.noreply.github.com>
2023-05-21 21:44:52 -0700

    Time: 82 ms (11.14%), Space: 43.2 MB (6.30%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,21 +1,17 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int diff = Integer.MAX_VALUE;
-        int len = nums.length;
+        int sz = nums.length;
         Arrays.sort(nums);
-        for (int i = 0; i < len && diff != 0; ++i) {
-            int low = i + 1;
-            int high = len - 1;
-            while (low < high) {
-                int sum = nums[i] + nums[low] + nums[high];
-                if (Math.abs(target - sum) < Math.abs(diff)) {
-                    diff = target - sum;
-                }
-                if (sum < target) {
-                    low++;
-                } else {
-                    high--;
-                }
+        for (int i = 0; i < sz && diff != 0; ++i) {
+            for (int j = i + 1; j < sz - 1; ++j) {
+                int complement = target - nums[i] - nums[j];
+                var idx = Arrays.binarySearch(nums, j + 1, sz - 1, complement);
+                int hi = idx >= 0 ? idx : ~idx, lo = hi - 1;
+                if (hi < sz && Math.abs(complement - nums[hi]) < Math.abs(diff))
+                    diff = complement - nums[hi];
+                if (lo > j && Math.abs(complement - nums[lo]) < Math.abs(diff))
+                    diff = complement - nums[lo];
             }
         }
         return target - diff;
