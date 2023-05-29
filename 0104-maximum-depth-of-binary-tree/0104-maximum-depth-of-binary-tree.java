966c2863b75772b7d6dfd5ac1f856b12c9f7d0ae
Kevin Kim <61047392+POL6463@users.noreply.github.com>
2023-05-28 21:19:19 -0700

    Time: 4 ms (11.16%), Space: 43.4 MB (8.88%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -1,33 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int maxDepth(TreeNode root) {
-        int res = countDepth(root);
-        return res;
-        
+  public int maxDepth(TreeNode root) {
+    LinkedList<TreeNode> stack = new LinkedList<>();
+    LinkedList<Integer> depths = new LinkedList<>();
+    if (root == null) return 0;
+
+    stack.add(root);
+    depths.add(1);
+
+    int depth = 0, current_depth = 0;
+    while(!stack.isEmpty()) {
+      root = stack.pollLast();
+      current_depth = depths.pollLast();
+      if (root != null) {
+        depth = Math.max(depth, current_depth);
+        stack.add(root.left);
+        stack.add(root.right);
+        depths.add(current_depth + 1);
+        depths.add(current_depth + 1);
+      }
     }
-    
-    public int countDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int left = countDepth(root.left);
-        int right = countDepth(root.right);
-        return Math.max(left, right) + 1;
-    }
-    
-    
-}
+    return depth;
+  }
+};
