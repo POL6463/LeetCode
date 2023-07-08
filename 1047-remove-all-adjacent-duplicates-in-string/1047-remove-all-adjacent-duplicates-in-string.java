03789569d7f823cb872c95cda032bdef78c77978
Kevin Kim <61047392+POL6463@users.noreply.github.com>
2023-07-07 22:42:54 -0700

    Time: 12 ms (93.22%), Space: 44.2 MB (79.68%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        int sbLength = 0;
+        for (char character : s.toCharArray()) {
+            if (sbLength != 0 && character == sb.charAt(sbLength - 1)) {
+                sb.deleteCharAt(sbLength-- - 1);
+            } else {
+                sb.append(character);
+                sbLength++;
+            }
+        }
+        return sb.toString();
+    }
+}
